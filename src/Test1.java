24b28da0df4f66a8f0ec8f78230c5c05c7cc6196
Admin <Admin@Admin-PC>
2021-10-19 22:53:42 +0530

    add

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+	public static void main(String[] args)
+	{
+		System.out.println("My first project");
+		System.out.println("comment1");	
+		System.out.println("Demo");	
+		System.out.println("comment2");
+		
+		
+		
+		
+	}
+}
